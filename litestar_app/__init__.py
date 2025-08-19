8cfaf9530ee720d9dd6ee1614222fee00dcb33ed
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2025-08-19 17:01:55 +0200

    snip snap

--- litestar_app/__init__.py
+++ litestar_app/__init__.py
@@ -3,10 +3,31 @@ from litestar import Litestar
 from litestar_app.api import greet
 
 
-@asynccontextmanager
-async def lifespan(app: Litestar):
-    # Setup code here
-    yield
-    # Teardown code here
+def option1():
+    return Litestar([greet])
 
-app = Litestar([greet], lifespan=[lifespan])
+
+def option2():
+    @asynccontextmanager
+    async def lifespan(app: Litestar):
+        yield
+
+    return Litestar([greet], lifespan=[lifespan])
+
+
+def option3():
+    @asynccontextmanager
+    async def lifespan(
+        app_settings,
+        _app: Litestar,
+    ):
+        yield
+
+    return Litestar([greet], lifespan=[lifespan])
+
+
+# Update as desired.
+# Option1 throws a hypercorn.utils.LifespanTimeoutError
+# Option2, while syntactically correct, also throws a hypercorn.utils.LifespanTimeoutError
+# Option3, while wrong code, works
+app = option2()
